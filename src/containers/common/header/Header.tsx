a2a1d2ca10b120284a17282410dcaae68210640b
gsi-manuel <manuel@generalsoftwareinc.com>
2018-05-26 13:31:51 -0400

    fixed header for responsive menu

--- src/containers/common/header/Header.tsx
+++ src/containers/common/header/Header.tsx
@@ -4,21 +4,50 @@ import './Header.scss';
 
 const logo = require('../../../assets/images/512px-React-icon.svg.ico');
 export class Header extends Component {
+
+    constructor(props: any) {
+        super(props);
+
+        this.responsiveFunction = this.responsiveFunction.bind(this);
+    }
+    public responsiveFunction() {
+        let x = document.getElementById('main-menu');
+        if (x!.className === 'row header__topnav') {
+            x!.className += ' responsive';
+        } else {
+            x!.className = 'row header__topnav';
+        }
+    }
+
     render() {
         return (
             <div className="row">
                 <div className="row-col-12">
                     <div className="row header">
-                        <div className="row-col-1 ">
-                            <img src={logo} className="header__logo" alt="logo" />
+                        <div className="row-col-2">
+                            <div className="row">
+                                <div className="header__container-logo">
+                                    <Link to="/">
+                                        <img src={logo} className="header__logo" alt="logo" />
+                                    </Link>
+                                </div>
+                                <div className="header__container-icon">
+                                    <a 
+                                        href="javascript:void(0);" 
+                                        className="header__icon header__links" 
+                                        onClick={this.responsiveFunction}
+                                    >
+                                        <i className="fas fa-bars"/>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
-                        <div className="row-col-5 logo"/>
+                        <div className="row-col-4 logo"/>
                         <div className="row-col-6">
-                            <div className="row" id="main-menu">                                
+                            <div className="row header__topnav" id="main-menu">                                
                                 <Link className="row-col-4 header__links header__links--hover" to="/">
                                     <i className="fas fa-chart-line"/>   DASHBOARD
-                                </Link>
-                                 
+                                </Link>                                 
                                 <a className="row-col-2 header__links header__links--hover" href="#">
                                     <i className="fas fa-user"/>   users
                                 </a>
@@ -30,8 +59,7 @@ export class Header extends Component {
                                 </Link>
                                 <Link className="row-col-1 header__links header__links--hover" to="/login">
                                     <i className="fas fa-power-off"/>
-                                </Link>
-                                                             
+                                </Link>                                                             
                             </div>
                         </div>
                     </div>
